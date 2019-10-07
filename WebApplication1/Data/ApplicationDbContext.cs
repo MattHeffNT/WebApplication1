7fa1134ef76f08dbb186b3f9bf521c97c62f3979
MattHeffNT <37504532+MattHeffNT@users.noreply.github.com>
2019-10-08 01:51:37 +0930

    Add project files.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Coach> Coach { get; set; }
+        public virtual DbSet<Schedule> Schedule { get; set; }
+        public virtual DbSet<ScheduleMembers> ScheduleMembers { get; set; }
+    }
+}
